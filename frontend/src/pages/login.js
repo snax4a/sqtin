26507b2310592bbe74eb1a004ac7c2c232320e60
Szymon Sus <sus.szymon3@gmail.com>
2021-01-01 17:21:33 +0100

    Added login form validation

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { accountService } from '_services';
 import { useHistory, useLocation } from 'react-router-dom';
+import * as Yup from 'yup';
 import * as ROUTES from 'constants/routes';
 import { NavbarContainer } from '../containers/navbar';
 import { PageContainer } from '../containers/page';
@@ -14,11 +15,34 @@ export default function Login() {
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [validationErrors, setValidationErrors] = useState([]);
 
-  const isInvalid = password === '' || emailAddress === '';
+  const validationSchema = Yup.object().shape({
+    emailAddress: Yup.string().email('Email is invalid').required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
 
-  const handleLogin = (event) => {
+  const isInvalid = (fieldName) => validationErrors.some((el) => el.indexOf(fieldName) > -1);
+
+  const isFormValid = async () => {
+    setValidationErrors([]);
+
+    return validationSchema
+      .validate({ emailAddress, password }, { abortEarly: false })
+      .then(() => true)
+      .catch((err) => {
+        setValidationErrors(err.errors);
+        return false;
+      });
+  };
+
+  const handleLogin = async (event) => {
     event.preventDefault();
+
+    if (!(await isFormValid())) return;
+
     setSubmitting(true);
     setError('');
 
@@ -43,11 +67,19 @@ export default function Login() {
         <Form onSubmit={handleLogin} method="POST" style={{ padding: 40, width: 400 }}>
           <Form.Title>Login To Dashboard</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
+          {validationErrors.length > 0 && (
+            <Form.Error>
+              {validationErrors.map((e) => (
+                <p>{e}</p>
+              ))}
+            </Form.Error>
+          )}
 
           <Form.Input
             placeholder="Email address"
             value={emailAddress}
             onChange={({ target }) => setEmailAddress(target.value)}
+            className={isInvalid('Email') ? 'invalid' : ''}
           />
           <Form.Input
             type="password"
@@ -55,8 +87,9 @@ export default function Login() {
             autoComplete="off"
             placeholder="Password"
             onChange={({ target }) => setPassword(target.value)}
+            className={isInvalid('Password') ? 'invalid' : ''}
           />
-          <Form.Submit disabled={isInvalid} type="submit" className="btn-block">
+          <Form.Submit type="submit" className="btn-block">
             {isSubmitting && <Loader />}
             Login
           </Form.Submit>
