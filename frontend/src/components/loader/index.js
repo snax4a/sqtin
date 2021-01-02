6923f223be1a119445557d7ab81c615bd9113d23
Szymon Sus <sus.szymon3@gmail.com>
2021-01-02 12:17:23 +0100

    Updated loader styles

--- frontend/src/components/loader/index.js
+++ frontend/src/components/loader/index.js
@@ -3,8 +3,10 @@ import './styles/loader.scss';
 
 export default function Loader(props) {
   return (
-    <svg className="loader" viewBox="0 0 50 50" {...props}>
-      <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
-    </svg>
+    <div className="loader-container">
+      <svg className="loader" viewBox="0 0 50 50" {...props}>
+        <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
+      </svg>
+    </div>
   );
 }
